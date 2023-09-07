e3738bda832af63bcd17fbcd128072e669b98e3d
Mitu Adrian <adrianmitu83@gmail.com>
2023-09-07 14:19:52 +0300

    add button for changing the sqaures-div

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const container = document.querySelector('#squares-container');
+const mainContainer = document.querySelector('#main-container');
+
+const changeButton = document.querySelector('#change-grid-button');
+// container.append(changeButton);
 
 for(i=0; i<16; i++){
     const lineContainer = document.createElement('div');
@@ -6,9 +10,17 @@ for(i=0; i<16; i++){
     container.appendChild(lineContainer);
     for(j=0; j<16; j++){
         const square = document.createElement('div');
-        square.classList.add('content');
+        square.classList.add('square');
+
+        square.addEventListener('mouseover', () => {
+            //alert("Hello World");
+            square.style.cssText = "background-color: blue;"
+          });
 
         lineContainer.appendChild(square);
     }
 }
+
+
+
 
