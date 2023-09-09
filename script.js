1b6dddf3aefa2b7cce026a4672006fe27b2cc457
Mitu Adrian <adrianmitu83@gmail.com>
2023-09-09 11:37:49 +0300

    made change-div to remove an add a new div with another dimension

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const container = document.querySelector('#squares-container');
-const mainContainer = document.querySelector('#main-container');
+const mainContainer = document.querySelector('.main-container');
+let container = document.querySelector('#squares-container');
 
 const changeButton = document.querySelector('#change-grid-button');
-// container.append(changeButton);
 
 for(i=0; i<16; i++){
     const lineContainer = document.createElement('div');
@@ -13,13 +12,49 @@ for(i=0; i<16; i++){
         square.classList.add('square');
 
         square.addEventListener('mouseover', () => {
-            //alert("Hello World");
             square.style.cssText = "background-color: blue;"
           });
 
         lineContainer.appendChild(square);
     }
 }
+
+function changeDivSize(sideDimension){
+    container.remove();
+
+    const newContainer = document.createElement('div');
+    newContainer.classList.add('new-container');
+    //mainContainer.appendChild(newContainer);
+
+    for(i=0; i<sideDimension; i++){
+        const lineContainer = document.createElement('div');
+        lineContainer.classList.add('line-container');
+        newContainer.appendChild(lineContainer);
+        for(j=0; j<sideDimension; j++){
+            const square = document.createElement('div');
+            square.classList.add('square');
+    
+            square.addEventListener('mouseover', () => {
+                square.style.cssText = "background-color: blue;"
+              });
+    
+            lineContainer.appendChild(square);
+        }
+    }
+
+    mainContainer.appendChild(newContainer);
+
+}
+
+changeButton.addEventListener('click', () =>{
+    let sideDimension = prompt('Insert the number of squares per side: ', 1);
+    while(sideDimension > 100 || sideDimension <0){
+        sideDimension = prompt('Please select an integer between 1 and 100', 1);
+    }
+
+    changeDivSize(sideDimension);
+
+})
 
 
 
