9c38f5771dc7cbb88e35413a87d0d2cb797bc3ec
Mitu Adrian <adrianmitu83@gmail.com>
2023-09-16 14:45:06 +0300

    added functionality to change the color of the pen in random color

--- script.js
+++ script.js
@@ -71,18 +71,24 @@ changeButton.addEventListener('click', () =>{
     changeDivSize(sideDimension);
 })
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
 randomColorButton.addEventListener('click', () =>{
     const pixels = document.querySelectorAll(".square");
     pixels.forEach(pixel => {
+        
+
         pixel.addEventListener('mouseover', () => {
-            pixel.style.backgroundColor = 'purple';
-            pixel.style.height = newDim;
-            pixel.style.width = newDim;
+            const randomColor = getRandomColor();
+            pixel.style.backgroundColor = randomColor;
+
             });
       });
 })
-
-
-
-
